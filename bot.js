c32d7b822a9e694b9b9aeeea5121b2fce9e0d4b0
Diego <diego2000avelar@gmail.com>
2021-08-20 20:48:48 -0600

    testing telegram bot

--- bot.js
+++ bot.js
@@ -3,10 +3,27 @@ const { bot_token } = require('./config/config');
 
 const bot = new Telegraf(bot_token);
 
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => {
+    console.log("subtype: ",ctx.updateType);
+    ctx.reply(`Welcome ${ctx.from.first_name} ${ctx.from.last_name}`)
+});
 
 bot.help((ctx) => ctx.reply('Help!!'));
 
 bot.settings((ctx) => ctx.reply('settings'));
+
+bot.command(['custom', 'mycomand'], (ctx) => ctx.reply('custom command'));
+
+bot.hears('computer', (ctx) => ctx.reply('hey i need a laptop'));
+
+// bot.on('text', (ctx) => ctx.reply('you are typing'));
+
+bot.on('stickered', (ctx) => ctx.reply('sticker!!'));
+
+bot.phone('+50376540104', (ctx) => ctx.reply('hey dude'))
+
+bot.mention('BotFather', (ctx) => ctx.reply('hey what happened'));
+
+bot.hashtag('programming', (ctx) => ctx.reply('hagstag programming'));
 
 bot.launch();
